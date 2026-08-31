695ff945431880b47e408512a0515eb163cc4e61
sbhoora <shubhibhoora@gmail.com>
2026-08-31 13:26:27 -0400

    updated some logic, added tailwindcss styling

--- src/Wordle.tsx
+++ src/Wordle.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 function Wordle() {
-    // keep track of correctly placed, misplaced, and incorrect letters
+    // dictionary api to get random secretWord, and check if guess is a real word
     const [letters, setLetters] = useState(Array(36).fill(''));
     const [guessCount, setGuessCount] = useState(0);
     const secretWord = 'SHUBHI';
@@ -13,42 +13,58 @@ function Wordle() {
                 console.log('Current guess:', currentGuess);
                 if (currentGuess.length === 6) {
                     if (currentGuess === secretWord) {
-                        console.log('You guessed the word!');
+                        // end game early if the guess is correct
+                        alert('You guessed the word!');
+                        setGuessCount(() => {
+                            const nextGuess = 6;
+                            return nextGuess;
+                        });
                     } else {
-                        console.log('Incorrect guess. Try again.');
+                        if (guessCount >= 5) {
+                            alert('Game over. The secret word was: ' + secretWord);
+                        } else {
+                            console.log('Incorrect guess. Try again.');
+                            // keep track of correctly placed, misplaced, and incorrect letters
+                        }
+                        setGuessCount(prev => {
+                            const nextGuess = prev + 1;
+                            return nextGuess;
+                        });
                     }
-                    setGuessCount(prev => {
-                        const nextGuess = prev + 1;
-                        return nextGuess;
-                    }); 
                 }
-            } else {
+            } else if (guessCount < 6) {
                 setLetters((prevLetters) => {
                     const newLetters = [...prevLetters];
                     const index = newLetters.findIndex(item => item === '');
                     if (e.key === 'Backspace' && index > guessCount * 6 && index <= guessCount * 6 + 6) {
-                        newLetters[index-1] = '';
-                    } else if (index <= guessCount * 6 + 5 && e.key.match(/[a-z]/i)) {
+                        newLetters[index - 1] = '';
+                    } else if (index <= guessCount * 6 + 5 && e.key.match(/^[a-z]$/i)) {
                         newLetters[index] = e.key.toUpperCase();
                     }
                     return newLetters;
-                }); 
+                });
             }
         }
         window.addEventListener('keydown', handleKeyDown);
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, [guessCount, letters]);
+    }, [guessCount, letters]); // Dependancy Array to re-run useEffect when guessCount or letters change
 
-    return(
+    return (
         <>
-            <div>Wordle</div>
-            <div className="grid grid-cols-6 gap-4">
-                {letters.map((letter, index) => (
-                    <div id={`cell-${index}`} key={index}>{letter}</div>
-                ))}
+            <div className="text-4xl font-bold mb-4 pt-4">Wordle</div>
+            <div className="items-center flex flex-col h-screen">
+                {/* add tile effects when ENTER is pressed and when a letter is typed */}
+                <div className="grid grid-cols-6 gap-x-1 gap-y-3 w-full max-w-sm">
+                    {letters.map((letter, index) => (
+                        <div id={`cell-${index}`} key={index} className="h-15 w-15 border-2 border-white/15  bg-white/5 text-white flex items-center justify-center font-bold text-2xl select-none">
+                            {letter}
+                        </div>
+                    ))}
+                </div>
             </div>
+
         </>
     )
 }
